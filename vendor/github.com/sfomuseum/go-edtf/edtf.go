b6f25b206df0d034147f8d148aabb3d8e3c9d20c
whosonfirst <whosonfirst@localhost>
2022-08-10 14:15:44 -0700

    update vendor deps

--- vendor/github.com/sfomuseum/go-edtf/edtf.go
+++ vendor/github.com/sfomuseum/go-edtf/edtf.go
@@ -1,6 +1,7 @@
 package edtf
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -8,13 +9,51 @@ const UNCERTAIN string = "?"
 const APPROXIMATE string = "~"
 const UNCERTAIN_AND_APPROXIMATE string = "%"
 const OPEN string = ".."
-const UNKNOWN string = ""
+const OPEN_2012 string = "open"
+const UNSPECIFIED string = ""
+const UNSPECIFIED_2012 string = "uuuu"
+const UNKNOWN string = UNSPECIFIED // this code was incorrectly referring to "unspecified" as "unknown"
+const UNKNOWN_2012 string = UNSPECIFIED_2012
+
 const NEGATIVE string = "-"
 
 const HMS_LOWER string = "00:00:00"
 const HMS_UPPER string = "23:59:59"
 
 const MAX_YEARS int = 9999 // This is a Golang thing
+
+// Return a boolean value indicating whether a string is considered to be an "open" EDTF date.
+func IsOpen(s string) bool {
+
+	switch s {
+	case OPEN, OPEN_2012:
+		return true
+	default:
+		return false
+	}
+}
+
+// Return a boolean value indicating whether a string is considered to be an "unspecified" EDTF date.
+func IsUnspecified(s string) bool {
+
+	switch s {
+	case UNSPECIFIED, UNSPECIFIED_2012:
+		return true
+	default:
+		return false
+	}
+}
+
+// Return a boolean value indicating whether a string is considered to be an "unknown" EDTF date.
+func IsUnknown(s string) bool {
+
+	switch s {
+	case UNKNOWN, UNKNOWN_2012:
+		return true
+	default:
+		return false
+	}
+}
 
 type EDTFDate struct {
 	Start   *DateRange `json:"start"`
@@ -56,4 +95,56 @@ do for now (20201223/thisisaaronland)
 
 func (d *EDTFDate) String() string {
 	return d.EDTF
+}
+
+// After reports whether the EDTFDate instance `d` is after `u`.
+func (d *EDTFDate) After(u *EDTFDate) (bool, error) {
+
+	if IsOpen(d.EDTF) {
+		return false, nil
+	}
+
+	u_t, err := u.Upper()
+
+	if err != nil {
+		return false, fmt.Errorf("Failed to derive upper time for inception date (%s), %w", u.EDTF, err)
+	}
+
+	t, err := d.Lower()
+
+	if err != nil {
+		return false, fmt.Errorf("Failed to derive lower time for cessation date (%s), %w", d.EDTF, err)
+	}
+
+	if u_t.After(*t) {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+// Before reports whether the EDTFDate instance `d` is after `u`.
+func (d *EDTFDate) Before(u *EDTFDate) (bool, error) {
+
+	if IsOpen(d.EDTF) {
+		return false, nil
+	}
+
+	u_t, err := u.Lower()
+
+	if err != nil {
+		return false, fmt.Errorf("Failed to derive lower time for inception date (%s), %w", u.EDTF, err)
+	}
+
+	t, err := d.Upper()
+
+	if err != nil {
+		return false, fmt.Errorf("Failed to derive upper time for cessation date (%s), %w", d.EDTF, err)
+	}
+
+	if u_t.Before(*t) {
+		return false, nil
+	}
+
+	return true, nil
 }
